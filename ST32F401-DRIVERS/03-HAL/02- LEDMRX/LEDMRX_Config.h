d509ef140c7d14e6e495ea4d9a110f6ef706af85
mostafa emary <mostafa.emary255@gmail.com>
2024-01-11 08:13:14 +0200

    ledmatrix drivers

---
+++ ST32F401-DRIVERS/03-HAL/02- LEDMRX/LEDMRX_Config.h
@@ -0,0 +1,27 @@
+/* ***************************************************************** */
+/* Author      : Mostafa Emary                                       */
+/* Date        : 11 Jan 2024                                         */
+/* Version     : V01                                                 */
+/* ***************************************************************** */
+#ifndef LEDMRX_Config_H
+#define LEDMRX_Config_H
+#define ROW0	GPIOA,P0
+#define ROW1	GPIOA,P1
+#define ROW2	GPIOA,P2
+#define ROW3	GPIOA,P3
+#define ROW4	GPIOA,P4
+#define ROW5	GPIOA,P5
+#define ROW6	GPIOA,P6
+#define ROW7	GPIOA,P7
+
+#define COL0	GPIOB,P0
+#define COL1	GPIOB,P1
+#define COL2	GPIOB,P5
+#define COL3	GPIOB,P6
+#define COL4	GPIOB,P7
+#define COL5	GPIOB,P8
+#define COL6	GPIOB,P9
+#define COL7	GPIOB,P10
+
+
+#endif
